a7d9b44aaa3fbb2c41a9bc404e69266c5422e5d8
Jconners23 <john.connolly@ada.ac.uk>
2025-03-13 22:58:17 +0000

    JWT implemented

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
+dotenv.config();
 app.use(express.json());
 
 const users = [];
 
+const posts = [
+    {
+        username: 'John',
+        title: 'post1'
+    },
+    {
+        username: 'Dave',
+        title: 'post2'
+    }
+];
+
+let refreshTokens = [];
+
 app.get('/users', (req, res) => {
     res.json(users);
+})
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name ));
 })
 
 app.post('/users', async (req, res) => {
@@ -24,14 +44,31 @@ app.post('/users', async (req, res) => {
     }
 })
 
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) { return res.sendStatus(401) };
+    if (!refreshTokens.includes(refreshToken)) { return res.sendStatus(403) };
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) { return res.sendStatus(403) };
+        const accessToken = generateAccessToken({ name: user.name });
+        res.json({ accessToken: accessToken });
+    })
+})
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token != req.body.token);
+    res.sendStatus(204);
+})
+
 app.post('/users/login', async (req, res) => {
     const user = users.find(user => user.name = req.body.name);
-    if (user == null) {
-        return res.status(400).send('Cannot find user');
-    }
+    if (user == null) { return res.status(400).send('Cannot find user'); }
     try {
         if (await bcrypt.compare(req.body.password, user.password)) {
-            res.send('Success');
+            const accessToken = generateAccessToken(user);
+            const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+            refreshTokens.push(refreshToken);
+            res.json({ accessToken: accessToken, refreshToken: refreshToken });
         } else {
             res.send('Not Allowed');
         }
@@ -39,5 +76,21 @@ app.post('/users/login', async (req, res) => {
         res.status(500).send();
     }
 })
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
+}
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) { return res.sendStatus(401); }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) { return res.sendStatus(403) }
+        req.user = user;
+        next();
+    });
+}
 
 app.listen(3000);
